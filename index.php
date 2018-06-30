56700a9eb0ecb1ea3b86161b41cd4cb43f5239cb
tinorus <>
2018-06-30 16:51:08 +0200

    Intialisation du projet en PHP

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>RedFire</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+<?php
+# INIT
+  @session_start();
+
+
+
+ 
+?>
+    
+
+        <div class="footer"> 
+            <p>Site du projet Fil Rouge de DUPORTAIL/GOUPIL/ROSE - 2018</p>
+        </div>
+    </body>
+</html>
+
