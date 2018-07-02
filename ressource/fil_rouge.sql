c961fba4c70ae53868ff5c1f17d2c8fff62e7754
tinorus <>
2018-07-02 21:52:47 +0200

    sql

--- ressource/fil_rouge.sql
+++ ressource/fil_rouge.sql
@@ -11,9 +11,18 @@
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
   `date_creation` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `admin`, `date_creation`) VALUES
+(1, 'test', 'test', 0, '2018-06-29'),
+(3, 'tinorus', 'test', 1, '2018-06-01');
 
 --
 -- Index pour les tables déchargées
@@ -33,5 +42,5 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
